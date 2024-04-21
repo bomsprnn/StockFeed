7d3bc5961936dced66d7b0bf3df404d6764631b3
bomsprnn <qhm12140525@gmail.com>
2024-04-21 15:10:07 +0900

    feat) newsfeed entity

--- src/main/java/com/example/stockfeed/Domain/NewsFeed.java
+++ src/main/java/com/example/stockfeed/Domain/NewsFeed.java
@@ -1,4 +1,46 @@
 package com.example.stockfeed.Domain;
 
-public class NewsFeed {
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class NewsFeed extends BaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "newsfeed_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; //뉴스피드 주인
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "own_user_id")
+    private User ownUser;
+    //컨텐츠 게시자 (게시글, 댓글 작성자), 혹은 팔로우-팔로잉 대상
+    @Enumerated(EnumType.STRING)
+    private NewsFeedType type; //뉴스피드 타입
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post; //게시글
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment; //댓글
+
+    @Builder
+    public NewsFeed(User user, User ownUser, NewsFeedType type, Post post, Comment comment) {
+        this.user = user;
+        this.ownUser = ownUser;
+        this.type = type;
+        this.post = post;
+        this.comment = comment;
+    }
+
+
+
 }
---
+++ src/main/java/com/example/stockfeed/Domain/NewsFeedType.java
@@ -0,0 +1,8 @@
+package com.example.stockfeed.Domain;
+
+
+public enum NewsFeedType {
+    POST, COMMENT,
+    POSTLIKE, COMMENTLIKE,
+    FOLLOW
+}

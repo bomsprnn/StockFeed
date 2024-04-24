c3440e4fb9f1623f87d1d6874523824656bff684
bomsprnn <qhm12140525@gmail.com>
2024-04-24 11:16:15 +0900

    .

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -3,4 +3,4 @@ insert into user
 values (now(), 'admin@localhost.com',  now(), 'host', '$2a$10$0Y6WozbD2Ot0YH5J86XfROIN3W.gEG0IPl/3wpoNo0SuMWI2pj1Tu', null, null, 'ROLE_USER');
 
 insert into follow (created_at, follower_id, following_id) values (now(), 2, 1);
-delete from follow where follow_id =3;
+delete from follow where follow_id =4;
